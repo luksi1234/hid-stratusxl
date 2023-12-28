44442c00f15069830d3ad7acc28e03c246d02d66
luksi1234 <luksi1234@gmail.com>
2023-12-28 08:11:36 +0100

    Update retropie_install.sh

--- install/retropie_install.sh
+++ install/retropie_install.sh
@@ -1,17 +1,20 @@
 !#/bin/bash
 
 DIRECTORY=/home/pi/RetroPie-Setup/scriptmodules/supplementary
-FILE=/tmp/repropie_install.sh
+FILE=/tmp/stratusxl.sh
 
 echo Download and install module script for Retropie Setup
 
 if [ -d "$DIRECTORY" ]; then
   echo "$DIRECTORY does exist."
-  wget -O /tmp/repropie_install.sh https://github.com/luksi1234/stratusxl/raw/main/install/repropie_install.sh
+  wget -O /tmp/stratusxl.sh https://raw.githubusercontent.com/luksi1234/stratusxl/main/RetroPie-Setup/scriptmodules/supplementary/stratusxl.sh
+else
+  echo "$DIRECTORY does not exist."
+  exit
 fi
 
 if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+    echo "$FILE exists."  
     mv $FILE $DIRECTORY
 else 
     echo "$FILE does not exist."
